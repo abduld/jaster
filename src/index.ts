74de2bc0e83a4c406b21c9c9c8cd33484b8ee86e
abdul dakkak <adakkak@gmail.com>
2014-11-13 00:06:49 -0600

    box now renders

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import Utils = require("./utils");
+import React = require("react");
+//import Utils = require("./utils");
 import Visualization = require("./visualization");
 import Memory = require("./memory");
 
-var main = () => {
+export var main = () => {
     var hostMemoryManager = new Memory.HostMemoryManager();
     var mem = hostMemoryManager.malloc(20);
+    console.log(Visualization.cell);
     console.log(mem);
-    React.renderToStaticMarkup(Visualization.cell({threadGroup: Utils.guuid()}));
+    var dom = document.getElementById("content");
+    var rect = React.DOM.svg(null, React.DOM.rect({width: 100, height: 100, fill: "black"}));
+    console.log(React.render(rect, dom));
+    console.log(React.renderToString(rect));
 };
 
-window.onload = () => {
-    main();
-};
