7cde10e759b460e57f00033108aaef7b98c98aac
abdul dakkak <adakkak@gmail.com>
2014-11-26 20:08:03 -0600

    add error implementation

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,1 +1,21 @@
 ﻿
+module lib.utils {
+    export module detail {
+        export enum ErrorCode {
+            Success,
+            MemoryOverflow,
+            IntegerOverflow,
+            Unknown
+        }
+    };
+    export class Error {
+        code : detail.ErrorCode;
+        constructor(code ? : detail.ErrorCode) {
+            if (code) {
+                this.code = code;
+            } else {
+                this.code = detail.ErrorCode.Success;
+            }
+        }
+    }
+}
