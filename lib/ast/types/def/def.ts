3dc0afae4a2879ad466f2e4e30855009c0b2798e
abdul dakkak <adakkak@gmail.com>
2014-11-29 16:55:36 -0600

    stubs

--- lib/ast/types/def/def.ts
+++ lib/ast/types/def/def.ts
@@ -1,3 +1,7 @@
-/**
- * Created by abduld on 11/27/14.
- */
+
+/// <referench path="core.ts" />
+/// <referench path="e4x.ts" />
+/// <referench path="es6.ts" />
+/// <referench path="es7.ts" />
+/// <referench path="fb-harmony.ts" />
+/// <referench path="mozilla.ts" />
